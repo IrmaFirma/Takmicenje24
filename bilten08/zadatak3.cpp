3ddb4a73551adf6692a147130b7aa68fb8b6a54d
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2024-02-25 17:16:07 +0100

    Update zadatak3.cpp

--- bilten08/zadatak3.cpp
+++ bilten08/zadatak3.cpp
@@ -1,26 +1,23 @@
+//Program koji pronalazi najveci neparni djelilac unesenog broja
+//Primjer n = 48 ispis je 3
+
+#include <cmath>
 #include <iostream>
 using namespace std;
-/* Napisi program koji ce ispisati najveci neparni djelilac ucitanog parnog prirodnog broja.*/
-//Primjer. 48 ispis je 3
 
 int main() {
-    // Unos parnog prirodnog broja
-    cout << "Unesite parni prirodan broj: ";
-    int broj;
-    cin >> broj;
 
-    // Provjera je li broj zaista paran
-    if (broj % 2 == 0) {
-        // Pronalaženje najvećeg neparnog djelioca
-        for (int i = broj / 2; i >= 1; i--) {
-            if (broj % i == 0 && i % 2 != 0) {
-                cout << "Najveci neparni djelilac: " << i << endl;
-                break;
-            }
-        }
-    } else {
-        cout << "Uneseni broj nije paran." << endl;
+  int n1;
+  int gcd = 0;
+  cin>>n1;
+
+  for(int i = n1; i>=2; i--){
+    if((n1%i==0) && (i%2!=0)) {
+      gcd = i;
     }
+  }
 
-    return 0;
+  cout<<gcd;
+
+  return 0;
 }
