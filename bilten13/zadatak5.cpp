b08b3658d1380e4ebc053cacbd8c49830af16898
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2024-03-26 13:34:33 +0100

    Update zadatak5.cpp

--- bilten13/zadatak5.cpp
+++ bilten13/zadatak5.cpp
@@ -1,7 +1,6 @@
 //Program kojim ispisuje sve proste palindromicne brojeve u intervalu od n - n*2 
 //Primjer n = 7 ispis = 7, 11
 
-
 #include <iostream>
 #include <string>
 #include <bits/stdc++.h>
@@ -9,39 +8,29 @@ using namespace std;
 
 bool jeProst(int n){
   bool jeProst = true;
-  for(int i = 2; i <= n/2; i++){  
+  for(int i = 2; i<n/2; i++){
     if(n%i==0) jeProst = false;
   }
+
   return jeProst;
-
 }
 
-bool jePalindrom(string b){
-  string p = b;
-  bool palindrom = true;
-
-  reverse(p.begin(), p.end());
-  if(b != p) palindrom = false;
-
-  return palindrom;
-}
 
 int main() {
 
-  int n; 
+  int n;
   cin>>n;
 
-  string b;
-
-  cout<<"Brojevi: "<<endl;
-  for(int i = n; i < 2*n; i++){
+  for(int i = n; i<=2*n; i++){
     if(jeProst(i)){
-      b = to_string(i);
-      if(jePalindrom(b)){
-        cout<<i<<endl;
+      string s = to_string(i);
+      reverse(s.begin(), s.end());
+      int new_i = stoi(s);
+      if(i == new_i){
+        cout<<i<<" ";
       }
     }
   }
   
-  return 0; 
+  return 0;
 }
