1ca0df6a4a213328bd0a0d7633a3c4925381611e
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2024-03-26 13:16:11 +0100

    Update zadatak2.cpp

--- bilten15/zadatak2.cpp
+++ bilten15/zadatak2.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
 #include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-/*Napisati program koji za 4 unesena cijela broja izvrsava slj trasnformacije: ako je max od ta 4 broja >= 500 tada se x i y umanjuje za 50 inace se svaki od brojeva x y z k umanji za svoju cifru jedinica. U prvom redu ispisati novodobijene brojeve a u drugom sumu novodobijenih brojeva*/
-//Primjer: 63 12 82 30 izlaz: 60 10 80 30 i 180
+
 
 int main() {
-    // Unos 4 cijela broja
-    cout << "Unesite 4 cijela broja: ";
-    int x, y, z, k;
-    cin >> x >> y >> z >> k;
 
-    // Provjera maksimalnog broja
-    int maxNumber = max({x, y, z, k});
+  int x, y, z, k;
+  cin>>x>>y>>z>>k;
 
-    if (maxNumber >= 500) {
-        x -= 50;
-        y -= 50;
-    } else {
-        x -= x % 10;
-        y -= y % 10;
-        z -= z % 10;
-        k -= k % 10;
-    }
+  if(max({x, y, z, k}) >=500){
+    x = x - 50;
+    y = y - 50;
+  }else{
+    x = x - (x%10);
+    y = y - (y%10);
+    z = z - (z%10);
+    k = k - (k%10);
+    cout<<x<<" "<<y<<" "<<z<<" "<<k<<endl;
+    cout<<x+y+z+k;
+  }
 
-    cout << "Novodobijeni brojevi: " << x << " " << y << " " << z << " " << k << endl;
-
-    int sum = x + y + z + k;
-    cout << "Suma novodobijenih brojeva: " << sum << endl;
-
-    return 0;
+  return 0;
 }
