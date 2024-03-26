a9d8aec6cb45d598a3ddb0718e61a84920d9a783
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2024-03-26 13:25:12 +0100

    Update zadatak2.cpp

--- bilten13/zadatak2.cpp
+++ bilten13/zadatak2.cpp
@@ -1,36 +1,39 @@
 //Program kojim se unosi n clanova niza, ako je suma clanova parna ispisati najveci clan niza ako nije ispisati najmanji
 //Primjer n = 3 clanovi su 1 4 7 ispis je 7 
-
-
 #include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+
 
 int main() {
 
-  int n; 
+  int n;
   cin>>n;
+
   int niz[n];
+
+  int suma = 0, max = niz[0], min = niz[1];
 
   for(int i = 0; i<n; i++){
     cin>>niz[i];
   }
 
-  int suma = 0;
-  int max = niz[0];
-  int min = niz[1];
-
   for(int i = 0; i<n; i++){
-    suma += niz[i];
-    if(niz[i]>max) max = niz[i];
-    if(niz[i]<min) min = niz[i];
+    suma = suma+niz[i];
   }
 
-  if(suma % 2 == 0){
+  if(suma%2==0){
+    for(int i = 0; i<n; i++){
+      if(niz[i]>max) max = niz[i];
+    }
     cout<<max;
   }else{
+    for(int i = 0; i<n; i++){
+      if(niz[i]<min) min = niz[i];
+    }
     cout<<min;
   }
-
-
-  return 0; 
+  
+  return 0;
 }
