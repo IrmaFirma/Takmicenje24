1e257199a29354f49103fcb3f3da95b29928998b
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2024-03-26 13:09:08 +0100

    Update zadatak1.cpp

--- bilten15/zadatak1.cpp
+++ bilten15/zadatak1.cpp
@@ -1,33 +1,19 @@
 #include <iostream>
+#include <string>
+#include <bits/stdc++.h>
 using namespace std;
-/*Napisati program kojim se za dati prirodan broj n formira broj i sastavljen od istih cifara ali u inverznom poretku*/
-//Primjer unos: 1234 ispis: 4321
+
 
 int main() {
-    // Unos broja n
-    cout << "Unesite prirodan broj n: ";
-    int n;
-    cin >> n;
 
-    // Provjera je li uneseni broj pozitivan
-    if (n <= 0) {
-        cout << "Uneseni broj mora biti pozitivan." << endl;
-        return 1;
-    }
+  int n;
+  cin>>n;
 
-    // Formiranje broja u inverznom poretku
-    int reversedNumber = 0;
-    int originalNumber = n;
+  string s = to_string(n);
 
-    while (originalNumber > 0) {
-        int digit = originalNumber % 10;
-        reversedNumber = reversedNumber * 10 + digit;
-        originalNumber /= 10;
-    }
+  reverse(s.begin(), s.end());
 
-    // Ispis rezultata
-    cout << "Broj sastavljen od istih cifara, ali u inverznom poretku: " << reversedNumber << endl;
-
-    return 0;
+  cout<<s;
+  return 0;
 }
 
