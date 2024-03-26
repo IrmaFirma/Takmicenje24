f8f71782fd0d39c394b3f9a2ffb4ca06c7ec77a5
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2024-03-26 13:20:55 +0100

    Update zadatak1.cpp

--- bilten13/zadatak1.cpp
+++ bilten13/zadatak1.cpp
@@ -2,27 +2,27 @@
 //Primjer x = 7.7 ispis je 77
 
 #include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+
 
 int main() {
 
-  double x;
+  double x, y = 0.0;
   cin>>x;
-
-  int y = 0;
 
   if(x<0){
     y = -10;
-  }else if(0<=x<1){
-    y = 1000 * x;
-  }else if(1<=x<10){
+  }else if(0<=x && x<1){
+    y = 1000*x;
+  }else if(1<=x && x<10){
     y = 10*x;
   }else if(x>=10){
     y = x/10;
   }
 
   cout<<y;
-
-
-  return 0; 
+  
+  return 0;
 }
