b878ad39e6df9b6df2b57aecd335d92e0763067c
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2024-03-31 18:18:43 +0200

    Update zadatak4.cpp

--- bilten17/zadatak4.cpp
+++ bilten17/zadatak4.cpp
@@ -1,29 +1,27 @@
 #include <iostream>
 using namespace std;
-/* napisati program bez koristenja posebnih funkcija za izracunavanje faktorijela prirodnog broja n pri cemu je 1<=n<=100 */
+/* napisati program za izracunavanje faktorijela prirodnog broja n pri cemu je 1<=n<=100 */
 // Primjer: za ulaz 6 ispis treba biti 720 
+#include <iostream>
+using namespace std;
 
 int main() {
-    // Unos broja n
-    int n;
-    cout << "Unesite prirodan broj n (1 <= n <= 100): ";
-    cin >> n;
 
-    if (n < 1 || n > 100) {
-        cout << "Neispravan unos. Broj n mora biti u opsegu od 1 do 100." << endl;
-        return 1;  
+  int n;
+  cin>>n;
+
+  int fact = 1;
+
+  if(n==1){
+    fact = 1;
+    cout<<fact;
+  }else{
+    for(int i = 2; i<=n; i++){
+      fact = fact*i; 
     }
+    cout<<fact;
+  }
 
-    // Inicijalizacija faktorijela na 1
-    long long faktorijel = 1;
-
-    // Računanje faktorijela
-    for (int i = 1; i <= n; ++i) {
-        faktorijel *= i;
-    }
-
-    // Ispis rezultata
-    cout << "Faktorijel broja " << n << " je: " << faktorijel << endl;
 
     return 0;
 }
